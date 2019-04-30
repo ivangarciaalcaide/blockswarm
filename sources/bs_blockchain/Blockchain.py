428649f8870d3e79d264e527b81d184a421f7b1b
Ivan Garcia-Alcaide <igarcia@etsisi.upm.es>
2019-04-30 12:35:20 +0200

    Blockchain.py docstrings translated into rst.

--- sources/bs_blockchain/Blockchain.py
+++ sources/bs_blockchain/Blockchain.py
@@ -1,12 +1,10 @@
-from jinja2.utils import generate_lorem_ipsum
-
-from bs_blockchain.Block import Block
 from abc import abstractmethod
 import binascii
 import json
 import time
 import gzip
 import pickle
+from bs_blockchain.Block import Block
 
 
 class Blockchain:
@@ -22,6 +20,7 @@ class Blockchain:
         Creates first block of the chain and append it to the chain.
 
         Considerations:
+
             - It is indexed at position 0.
             - As there is not a previous block, previous_hash is set to "0".
             - As there are not transactions to mine yet, transaction list is empty.
@@ -35,15 +34,14 @@ class Blockchain:
         Proof of work implementation.
 
         It takes a block and sets its nonce to 0. Then, calculates its hash from
-        L{Block.get_string_to_hash(self)<Block.get_string_to_hash(self)>}. This hash must
+        :py:meth:`.Block.get_string_to_hash`. This hash must
         end with, at least, as many '0's characters as indicated in
-        L{pow_difficulty} to success. I hash doesn't accomplish PoW, nonce value is
+        *pow_difficulty* to success. I hash doesn't accomplish PoW, nonce value is
         incremented by 1 and it starts over.
 
         Once nonce is found, block's hash is updated.
 
-        @param block: Block over the proof of work is executed.
-        # @return: String with the hash that accomplish the PoW.
+        :param block: Block over the proof of work is executed.
         """
         block.nonce = 0
         current_hash = block.calculate_hash()
@@ -55,10 +53,10 @@ class Blockchain:
 
     def add_new_transaction(self, transaction):
         """
-        Insert a L{Transaction} at the end of L{unconfirmed transactions<unconfirmed_transactions>}
+        Insert a :py:class:`.Transaction` at the end of :py:attr:`unconfirmed_transactions`
         list.
 
-        @param transaction: L{Transaction} object to be inserted.
+        :param transaction: :py:class:`.Transaction` object to be inserted.
         """
         self.unconfirmed_transactions.append(json.loads(str(transaction)))
 
@@ -67,7 +65,7 @@ class Blockchain:
         """
         It selects what transactions to be mined in a given block.
 
-        @return: A list with selected L{transactions<Transaction>}
+        :return: A list with selected :py:class:`.Transaction`
         """
 
     def mine(self, allow_empty_transactions=False):
@@ -75,19 +73,21 @@ class Blockchain:
         Mines a new block and add it to the chain.
 
         Mining steps:
+
             1. Selects transactions to be incorporated to the block.
             2. Creates a block with current information available
+
                 - Selected transactions
                 - Current time for timestamp
                 - Previous hash filled with last block hash.
+
             3. Fills hash field with proof of work.
-            4. Removes mined transactions from L{unconfirmed_transactions} list.
+            4. Removes mined transactions from :py:attr:`unconfirmed_transactions` list.
 
 
-        @type allow_empty_transactions: boolean
-        @param allow_empty_transactions: True, mine the block even if transaction's block list is empty.
+        :param allow_empty_transactions: True, mine the block even if transaction's block list is empty.
 
-        @return: False, If block could not be mined. The mined block, otherwise.
+        :return: False, If block could not be mined. The mined block, otherwise.
         """
         selected_transactions = self.select_transactions_to_mine()
         if not selected_transactions and not allow_empty_transactions:
@@ -115,14 +115,15 @@ class Blockchain:
         Checks if the block is a valid one.
 
         To be a valid block it has to accomplish following criteria:
+
             1. Its previous hash must be the same as previous block hash.
             2. Its index must be the following from previous block.
-            3. Its hash must end with at least L{pow_difficulty} numbers of '0's
+            3. Its hash must end with at least :py:attr:`pow_difficulty` numbers of '0's
             4. Its hash must be the same than calculated hash.
 
-        @param chain: Chain where the block is. If empty, it takes its own chain.
-        @param block: Block to be checked.
-        @return: False if not valid. True, otherwise.
+        :param chain: Chain where the block is. If empty, it takes its own chain.
+        :param block: Block to be checked.
+        :return: False if not valid. True, otherwise.
         """
         if chain is None:
             chain = []
@@ -147,10 +148,10 @@ class Blockchain:
 
     def is_valid_chain(self, chain):
         """
-        It checks if every block in the chain L{is_valid_block}.
+        It checks if every block in the chain :py:meth:'is_valid_block`.
 
-        @param chain: The chain to check
-        @return: True, if it is valid. False, otherwise.
+        :param chain: The chain to check
+        :return: True, if it is valid. False, otherwise.
         """
         x = 1
         while x < len(chain) and self.is_valid_block(chain[x], chain):
@@ -164,8 +165,8 @@ class Blockchain:
         """
         Append a new block to the chain after checking its correctness.
 
-        @param new_block: New block to be added.
-        @return: False, if block couldn't be added. The new block, otherwise.
+        :param new_block: New block to be added.
+        :return: False, if block couldn't be added. The new block, otherwise.
         """
         if self.is_valid_block(new_block):
             self.chain.append(new_block)
@@ -174,21 +175,20 @@ class Blockchain:
             return False
 
     def chain_to_file(self, filename, compress_level=0):
-
         """
         Saves the current chain into a file.
 
-        Depending on B{compress_level} it behaves in two different ways. B{compress_level} range is from 0 to 9. By default, B{compress_level} is set to 0:
-            - "B{0}": No compress and JSON text file is generated I{UTF-8} encoded.
-            - "B{1 - 9}": Compressed B{gzip} file is generated. B{1} is fastest and produces the least compression.
-                            B{9} is slowest and produces the most compression. File will not be human readable.
+        Depending on **compress_level** it behaves in two different ways. **compress_level** range is from 0 to 9.
+        By default, **compress_level** is set to 0:
+
+            - "**0**": No compress and JSON text file is generated *UTF-8* encoded.
+            - "**1 - 9**": Compressed **gzip** file is generated. **1** is fastest and produces the least compression.\
+                            **9** is slowest and produces the most compression. File will not be human readable.
 
         Working with text files is much harder and takes much longer than with binary (compressed) files.
 
-        @type filename: String
-        @param filename: Desired name for the file. (Path is relative to working dir)
-        @type compress_level: int
-        @param compress_level: Desired compress level or, 0, produces a JSON text file (UTF-8).
+        :param filename: Desired name for the file. (Path is relative to working dir)
+        :param compress_level: Desired compress level or, 0, produces a JSON text file (UTF-8).
         """
         if compress_level in range(1, 10):
             with gzip.open(filename, "wb", compresslevel=compress_level) as gzip_file:
@@ -201,9 +201,9 @@ class Blockchain:
 
     def chain_from_file(self, filename):
         """
-        Reads a file that contains a chain and store it as its chain if it is valid.
+        Reads a file (compressed or not) that contains a chain and store it as its chain if it is valid.
 
-        @param filename: Name of the file wher the chain is stored. (Path is relative to working dir)
+        :param filename: Name of the file where the chain is stored. (Path is relative to working dir)
         """
         with open(filename, "rb") as checking_file:
             is_gzip_file = binascii.hexlify(checking_file.read(2)) == b'1f8b'
