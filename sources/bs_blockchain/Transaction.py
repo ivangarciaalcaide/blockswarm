3fbc1d0ebe4b308e7915d873c7671f0af478bff0
Ivan Garcia-Alcaide <igarcia@etsisi.upm.es>
2019-04-30 12:41:05 +0200

    Transaction.py docstrings translated into rst.

--- sources/bs_blockchain/Transaction.py
+++ sources/bs_blockchain/Transaction.py
@@ -4,16 +4,16 @@ from hashlib import sha1
 
 
 class Transaction:
-
-    # TODO: Generated unique id is not unique as different Transaction objects could be hashed the same.
-    #  Is that a problem?
+    """
+    It represents a Transaction. It contains the data associated to the transaction and an unique id.
+    """
     def __init__(self, data, id_tx=""):
         if not id_tx:
             self.id_tx = sha1((json.dumps(data) + str(time.time())).encode()).hexdigest()  #: Generated unique Id
         else:
             self.id_tx = id_tx
 
-        self.data = json.loads(data)  #: Custom data for transaction in B{json} format
+        self.data = json.loads(data)  #: Custom data for transaction in **JSON** format
 
     def __str__(self):
         return json.dumps(self.__dict__)
